f0b7704d6b6b04b6bcee768c7365118a77163c9a
burgund <my.telmenos@gmail.com>
2024-04-12 11:52:56 +0200

    Modify ClickUpProperties.java for jenkins authorizations

--- src/main/java/pl/akademiaqa/properties/ClickUpProperties.java
+++ src/main/java/pl/akademiaqa/properties/ClickUpProperties.java
@@ -12,10 +12,18 @@ public class ClickUpProperties {
     private static final String TEAM_ID = "clickup.team.id";
 
     public static String getToken() {
+
+        if (getProperty(TOKEN).isEmpty() || getProperty(TOKEN).startsWith("YOUR")) {
+            return System.getProperty("TOKEN");
+        }
         return getProperty(TOKEN);
     }
 
     public static String getTeamId() {
+
+        if (getProperty(TEAM_ID).isEmpty() || getProperty(TEAM_ID).startsWith("YOUR")) {
+            return System.getProperty("TEAM_ID");
+        }
         return getProperty(TEAM_ID);
     }
 
